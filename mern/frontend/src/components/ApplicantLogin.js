d59a359c1d565bda48006db0fd7f77a9efffc2c6
Oishy Fatema Akhand <95131852+oishy28@users.noreply.github.com>
2023-10-24 22:35:46 +0600

    frontend fixing applicant signup , signin

--- mern/frontend/src/components/ApplicantLogin.js
+++ mern/frontend/src/components/ApplicantLogin.js
@@ -33,7 +33,9 @@ function ApplicantLogin() {
             borderRadius: '5px',
             boxShadow: '0px 0px 8px rgba(0, 0, 0, 0.1)',
             maxWidth: '400px',
-            margin: '50px auto'
+            margin: '50px auto',
+            background: '#111111',
+            text:'white'
         },
         input: {
             margin: '10px 0',
@@ -43,7 +45,7 @@ function ApplicantLogin() {
         },
         button: {
             padding: '10px 20px',
-            background: '#007BFF',
+            background: '#333333',
             color: '#fff',
             borderRadius: '5px',
             border: 'none',
@@ -54,7 +56,7 @@ function ApplicantLogin() {
 
     return (
         <div style={styles.container}>
-            <h2>Applicant Login</h2>
+            <h2 style={{ color: '#fff', fontWeight: 'bold' }}>Applicant Login</h2>
             <form onSubmit={handleSubmit}>
                 <input style={styles.input} type="email" name="email" placeholder="Email" onChange={handleChange} required />
                 <input style={styles.input} type="password" name="password" placeholder="Password" onChange={handleChange} required />
