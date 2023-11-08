980992767fc2d1b5fc3ff1c6d4802f5bbabcbbc3
Shadman Sakib <shadmansakib20@iut-dhaka.edu>
2023-11-08 14:16:08 +0600

    new

--- mern/client/src/components/Hero.jsx
+++ mern/client/src/components/Hero.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import Typed from 'react-typed';
 
 const Hero = () => {
+  const redirectToTarget = () => {
+    
+    window.location.href = 'http://localhost:3002';
+  };
+
   return (
     <div className='text-white'>
       <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
@@ -13,27 +18,29 @@ const Hero = () => {
         </h1>
         <div className='flex justify-center items-center'>
           <div>
-          <p className='md:text-4xl sm:text-3xl text-xl font-bold py-4'>
-            Smart solutions for
-          </p>
-
+            <p className='md:text-4xl sm:text-3xl text-xl font-bold py-4'>
+              Smart solutions for
+            </p>
           </div>
           <div>
-          <Typed
-          className='md:text-4xl sm:text-3xl text-xl font-bold md:pl-4 pl-2'
-            strings={['Resume Shortlisting', 'Resume Building']}
-            typeSpeed={120}
-            backSpeed={140}
-            loop
-          />
-
-
+            <Typed
+              className='md:text-4xl sm:text-3xl text-xl font-bold md:pl-4 pl-2'
+              strings={['Resume Shortlisting', 'Resume Building']}
+              typeSpeed={120}
+              backSpeed={140}
+              loop
+            />
           </div>
-         
-         
         </div>
-        <p className='md:text-2xl text-xl font-bold text-gray-500'>Streamline your resume management to find the best candidates and create outstanding resumes effortlessly.</p>
-        <button className='bg-[#989898] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
+        <p className='md:text-2xl text-xl font-bold text-gray-500'>
+          Streamline your resume management to find the best candidates and create outstanding resumes effortlessly.
+        </p>
+        <button
+          className='bg-[#989898] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'
+          onClick={redirectToTarget}
+        >
+          Build Resume
+        </button>
       </div>
     </div>
   );
