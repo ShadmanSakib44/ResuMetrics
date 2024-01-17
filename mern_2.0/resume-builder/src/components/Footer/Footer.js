20c59a70696b04030f2398e7d156235a57e3715b
ShadmanSakib44 <shadmansakib20@iut-dhaka.edu>
2024-01-17 17:59:11 +0600

    integration

---
+++ mern_2.0/resume-builder/src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './Footer.css';
+
+
+const Footer = () => {
+    return (
+        <footer className='Footer'>
+            <div className='ui-container'>
+                <a href='#' className='ui-link'>
+                    ResuMetrics
+                </a>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
